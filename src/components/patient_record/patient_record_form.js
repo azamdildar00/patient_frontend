aab45330f035ee37e49fede83b015e2148f59e07
najam <dev.najam@gmail.com>
2022-02-06 18:09:49 +0500

    updates

--- src/components/patient_record/patient_record_form.js
+++ src/components/patient_record/patient_record_form.js
@@ -1,104 +1,155 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import { Button, Container, Form, Row, Col } from 'react-bootstrap';
+import {
+  Button,
+  Container,
+  Form,
+  Row,
+  Col,
+  Spinner,
+  Table,
+} from "react-bootstrap";
 
 const PatientRecord = () => {
-    
   // const [patientName, setPatientName] = useState('')
   // const [patientDisease, setDiseaseName] = useState('')
-  const [searchPatient, setSearchPatient] = useState(false)
-  const [addPatient, setAddPatient] = useState(false)
-
-
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchPatient, setSearchPatient] = useState(false);
+  const [addPatient, setAddPatient] = useState(false);
+  const [patRecord, setPatRecord] = useState();
 
   const searchBtn = () => {
-    setSearchPatient(true)
-    setAddPatient(false)
-  }
+    setSearchPatient(true);
+    setAddPatient(false);
+  };
 
   const addBtn = () => {
-    setSearchPatient(false)
-    setAddPatient(true)
-  }
+    setSearchPatient(false);
+    setAddPatient(true);
+  };
 
   const handleSubmitSearch = (e) => {
     e.preventDefault();
-    console.log(e.target.elements.searchPatient.value);
-
-    axios.get(`https://patient-backend111.herokuapp.com/patient?name=${e.target.elements.searchPatient.value}`)
-      .then(res => console.log(res.data))
-  }
+    axios
+      .get(
+        `https://patient-backend111.herokuapp.com/patient?name=${e.target.elements.searchPatient.value}`
+      )
+      .then((res) => setPatRecord(res.data));
+  };
 
   const handleSubmit = (e) => {
+    setIsLoading(true);
     e.preventDefault();
-    console.log(e.target.elements.patientName.value);
-    console.log(e.target.elements.patientDisease.value);
-
     const patient = {
-      name : e.target.elements.patientName.value,
-      disease : e.target.elements.patientDisease.value
-    }
+      name: e.target.elements.patientName.value,
+      disease: e.target.elements.patientDisease.value,
+    };
 
-    axios.post('https://patient-backend111.herokuapp.com/patient', {patient})
-      .then(res => console.log(res));
+    axios
+      .post("https://patient-backend111.herokuapp.com/patient", { patient })
+      .then((res) => {
+        setIsLoading(false);
+      });
+  };
 
-    // console.log(patientName);
-    // console.log(patientDisease);
-  }
-
-    return (
-      <>
-        <Container>
-            <Row className="mt-5">
-                <Col lg={5} md={6} sm={12} className="p-5 m-auto shadow-sm rounded-lg">
-                    <Button variant="primary" onClick={searchBtn}>Search Search</Button>{' '}
-                    <Button variant="primary" onClick={addBtn}>Add Patient</Button>
-                </Col>
-            </Row>
-        </Container>
-        {searchPatient && 
-            <Container>
-                <Row className="mt-5">
-                    <Col lg={5} md={6} sm={12} className="p-5 m-auto shadow-sm rounded-lg">
-                    <Form onSubmit={handleSubmitSearch}>
-                    <Form.Group className="mb-3" controlId='searchPatient'>
-                        <Form.Label>Search Patient</Form.Label>
-                            <Form.Control type="text" placeholder="Patient Name"/>
-                        </Form.Group>
-                        <Button variant="primary" type="submit" >
-                            Search
-                        </Button>
-                    </Form>
-                    </Col>
-                </Row>
-            </Container>
-        }
-        {addPatient && 
-        <Container>
+  return (
+    <>
+      <Container>
         <Row className="mt-5">
-          <Col lg={5} md={6} sm={12} className="p-5 m-auto shadow-sm rounded-lg">
-          <h3 className="shadow-sm text-success mt-5 text-center rounded">Patient Record</h3>
-            <Form onSubmit={handleSubmit}>
-              <Form.Group className="mb-3" controlId='patientName'>
-                <Form.Label>Patient Name</Form.Label>
-                <Form.Control type="text" placeholder="Patient Name" />
-              </Form.Group>
-
-              <Form.Group className="mb-3" controlId='patientDisease'>
-                <Form.Label>Patient Disease</Form.Label>
-                <Form.Control type="text" placeholder="Disease Name"/>
-              </Form.Group>
-
-              <Button variant="primary" type="submit" >
-                Submit
-              </Button>
-            </Form>
+          <Col
+            lg={5}
+            md={6}
+            sm={12}
+            className="p-5 m-auto shadow-sm rounded-lg"
+          >
+            <Button variant="primary" onClick={searchBtn}>
+              Search Search
+            </Button>{" "}
+            <Button variant="primary" onClick={addBtn}>
+              Add Patient
+            </Button>
           </Col>
         </Row>
       </Container>
-        }
-      </>
-    )
-}
+      {searchPatient && (
+        <Container>
+          <Row className="mt-5">
+            <Col
+              lg={5}
+              md={6}
+              sm={12}
+              className="p-5 m-auto shadow-sm rounded-lg"
+            >
+              <Form onSubmit={handleSubmitSearch}>
+                <Form.Group className="mb-3" controlId="searchPatient">
+                  <Form.Label>Search Patient</Form.Label>
+                  <Form.Control type="text" placeholder="Patient Name" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Search
+                </Button>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      )}
+      {addPatient && (
+        <Container>
+          <Row className="mt-5">
+            <Col
+              lg={5}
+              md={6}
+              sm={12}
+              className="p-5 m-auto shadow-sm rounded-lg"
+            >
+              <h3 className="shadow-sm text-success mt-5 text-center rounded">
+                Patient Record
+              </h3>
+              <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="patientName">
+                  <Form.Label>Patient Name</Form.Label>
+                  <Form.Control type="text" placeholder="Patient Name" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="patientDisease">
+                  <Form.Label>Patient Disease</Form.Label>
+                  <Form.Control type="text" placeholder="Disease Name" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                  {isLoading ? <Spinner animation="border" /> : "Submit"}
+                </Button>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      )}
+      {patRecord && !addPatient && (
+        <Container>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Patient Name</th>
+                <th>Treatment Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {patRecord.map((r) => {
+                return (
+                  <tr>
+                    <td>{r.id}</td>
+                    <td>{r.name}</td>
+                    <td>{r.disease}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </Container>
+      )}
+    </>
+  );
+};
 
 export default PatientRecord;
--- src/components/treatment_record/treatment_record_form.js
+++ src/components/treatment_record/treatment_record_form.js
@@ -1,119 +1,169 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import { Button, Container, Form, Row, Col } from 'react-bootstrap';
+import { Button, Container, Form, Row, Col, Table } from "react-bootstrap";
 
 const TreatmentRecord = () => {
-//   const [patientName, setPatientName] = useState('')
-//   const [treatDisease, setTreatName] = useState('')
-    // const [treatRecord, setTreatRecord] = useState('')
+  // const [patientName, setPatientName] = useState('')
+  // const [treatDisease, setTreatName] = useState('')
+  const [treatRecord, setTreatRecord] = useState();
 
   const [check, setCheck] = useState(true);
-  const [searchtreat, setSearchTreat] = useState(false)
-  const [addtreat, setAddTreat] = useState(false)
+  const [searchtreat, setSearchTreat] = useState(false);
+  const [addtreat, setAddTreat] = useState(false);
 
   const handleChange = (e) => {
     e.preventDefault();
-    console.log(e.target.value)
-    if(e.target.value === "1")
-        setCheck(true)
-    else
-        setCheck(false)
-  }
+    console.log(e.target.value);
+    if (e.target.value === "1") setCheck(true);
+    else setCheck(false);
+  };
 
   const searchBtn = () => {
-    setSearchTreat(true)
-    setAddTreat(false)
-  }
+    setSearchTreat(true);
+    setAddTreat(false);
+  };
 
   const addBtn = () => {
-    setSearchTreat(false)
-    setAddTreat(true)
-  }
+    setSearchTreat(false);
+    setAddTreat(true);
+  };
 
   const handleSubmitSearch = (e) => {
-      e.preventDefault();
-      console.log(e.target.elements.searchDisease.value);
+    e.preventDefault();
+    console.log(e.target.elements.searchDisease.value);
 
-      axios.get(`https://patient-backend111.herokuapp.com/treatment?type=${e.target.elements.searchDisease.value}`)
-        .then(res => {
-            console.log(res.data)
-            // setTreatRecord(res.data);
-        })
-  }
+    axios
+      .get(
+        `https://patient-backend111.herokuapp.com/treatment?type=${e.target.elements.searchDisease.value}`
+      )
+      .then((res) => {
+        setTreatRecord(res.data);
+      });
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(e.target.elements.patientName.value);
     console.log(e.target.elements.patientDisease.value);
 
     const treatment = {
-        patient_name : e.target.elements.patientName.value,
-        disease : e.target.elements.patientDisease.value,
-        prescription : check
-      }
-  
-      axios.post('https://patient-backend111.herokuapp.com/treatment', {treatment})
-        .then(res => console.log(res.data));
-    }
+      patient_name: e.target.elements.patientName.value,
+      disease: e.target.elements.patientDisease.value,
+      prescription: check,
+    };
 
-    return (
-        <>
-        <Container>
-            <Row className="mt-5">
-                <Col lg={5} md={6} sm={12} className="p-5 m-auto shadow-sm rounded-lg">
-                    <Button variant="primary" onClick={searchBtn}>Search Treatment</Button>{' '}
-                    <Button variant="primary" onClick={addBtn}>Add Treatment</Button>
-                </Col>
-            </Row>
-        </Container>
-        {searchtreat && 
-            <Container>
-                <Row className="mt-5">
-                    <Col lg={5} md={6} sm={12} className="p-5 m-auto shadow-sm rounded-lg">
-                    <Form onSubmit={handleSubmitSearch}>
-                    <Form.Group className="mb-3" controlId='searchDisease'>
-                        <Form.Label>Search Treatment</Form.Label>
-                            <Form.Control type="text" placeholder="Disease Name"/>
-                        </Form.Group>
-                        <Button variant="primary" type="submit" >
-                            Search
-                        </Button>
-                    </Form>
-                    </Col>
-                </Row>
-            </Container>
-        }
-        {addtreat &&
-        <Container>
+    axios
+      .post("https://patient-backend111.herokuapp.com/treatment", { treatment })
+      .then((res) => console.log(res.data));
+  };
+
+  return (
+    <>
+      <Container>
         <Row className="mt-5">
-          <Col lg={5} md={6} sm={12} className="p-5 m-auto shadow-sm rounded-lg">
-          <h3 className="shadow-sm text-success mt-5 text-center rounded">Treatment Record</h3>
-            <Form onSubmit={handleSubmit}>
-              <Form.Group className="mb-3" controlId='patientName'>
-                <Form.Label>Patient Name</Form.Label>
-                <Form.Control type="text" placeholder="Patient Name" />
-              </Form.Group>
-
-              <Form.Group className="mb-3" controlId='patientDisease'>
-                <Form.Label>Patient Disease</Form.Label>
-                <Form.Control type="text" placeholder="Disease Name"/>
-              </Form.Group>
-
-              <Form.Select className="mb-3" aria-label="Default select example" onChange={handleChange}>
-                <option>Prescription issued?</option>
-                <option value="1">Yes</option>
-                <option value="2">No</option>
-            </Form.Select>
-
-              <Button variant="primary" type="submit" >
-                Submit
-              </Button>
-            </Form>
+          <Col
+            lg={5}
+            md={6}
+            sm={12}
+            className="p-5 m-auto shadow-sm rounded-lg"
+          >
+            <Button variant="primary" onClick={searchBtn}>
+              Search Treatment
+            </Button>{" "}
+            <Button variant="primary" onClick={addBtn}>
+              Add Treatment
+            </Button>
           </Col>
         </Row>
       </Container>
-        }
-      </>
-    )
-}
+      {searchtreat && (
+        <Container>
+          <Row className="mt-5">
+            <Col
+              lg={5}
+              md={6}
+              sm={12}
+              className="p-5 m-auto shadow-sm rounded-lg"
+            >
+              <Form onSubmit={handleSubmitSearch}>
+                <Form.Group className="mb-3" controlId="searchDisease">
+                  <Form.Label>Search Treatment</Form.Label>
+                  <Form.Control type="text" placeholder="Disease Name" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Search
+                </Button>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      )}
+      {addtreat && (
+        <Container>
+          <Row className="mt-5">
+            <Col
+              lg={5}
+              md={6}
+              sm={12}
+              className="p-5 m-auto shadow-sm rounded-lg"
+            >
+              <h3 className="shadow-sm text-success mt-5 text-center rounded">
+                Treatment Record
+              </h3>
+              <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="patientName">
+                  <Form.Label>Patient Name</Form.Label>
+                  <Form.Control type="text" placeholder="Patient Name" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="patientDisease">
+                  <Form.Label>Patient Disease</Form.Label>
+                  <Form.Control type="text" placeholder="Disease Name" />
+                </Form.Group>
+
+                <Form.Select
+                  className="mb-3"
+                  aria-label="Default select example"
+                  onChange={handleChange}
+                >
+                  <option>Prescription issued?</option>
+                  <option value="1">Yes</option>
+                  <option value="2">No</option>
+                </Form.Select>
+
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      )}
+      {treatRecord && !addtreat && (
+        <Container>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Patient Name</th>
+                <th>Treatment Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {treatRecord.map((r) => {
+                return (
+                  <tr>
+                    <td>{r.id}</td>
+                    <td>{r.patient_name}</td>
+                    <td>{r.disease}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </Container>
+      )}
+    </>
+  );
+};
 
 export default TreatmentRecord;
